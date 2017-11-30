ae1ba5ce5a80927b22d60be15200b9ea1b738390
Lito Soler <lito.soler1@gmail.com>
2017-11-29 23:17:22 -0600

    pagina de trabajando añadida, actualmente index

---
+++ app.js
@@ -0,0 +1,14 @@
+//objetos de estado
+
+
+//funciones para el manejo del estado
+
+
+//funcioens para el manejo del domm
+
+
+//funciones de accion
+
+$(function(){
+	$("#mensaje").css("margin-top", ($(window).height() / 2) - 100);
+});
