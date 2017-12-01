73e7d7fab7fe8bc3c952bba918dfd0ac0172f8f9
Lito Soler <lito.soler1@gmail.com>
2017-12-01 11:16:16 -0600

    menu del home terminado, y mensaje oculto añadido

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 //objetos de estado
-
+estado = {homeClicks:1,}
 
 //funciones para el manejo del estado
-
+function AumentarHomeClicks(estado){
+	estado.homeClicks = estado.homeClicks + 1;
+}
 
 //funcioens para el manejo del domm
 
@@ -12,4 +14,31 @@ $(function(){
 	$("#mensaje").css("margin-top", ($(window).height() / 2) - 100);
 });
 
-$("header").hover(alert());
+$("header").hover(function(){
+	$(this).addClass("hover");
+}, function(){
+	$(this).removeClass("hover");
+});
+
+$("li a").hover(function(){
+	if( !($(this).html() === "Home")){
+		$(this).addClass("not-allowed")
+		$("#no-disponible").show();
+	}
+},function(){
+	$(this).removeClass("not-allowed")
+	$("#no-disponible").hide();
+
+});
+
+$("li a").click(function(){
+	if( $(this).html() === "Home"){
+	$("li a").removeClass("active");
+	$(this).addClass("active");
+	AumentarHomeClicks(estado);
+	if(estado.homeClicks == 5){
+		$("#yariela").fadeIn(2000, "swing");	
+	}
+	}
+});
+
