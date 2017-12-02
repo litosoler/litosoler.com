f87b6304e9f5e79cbc836d1d5ca2506c49936dd9
Lito Soler <lito.soler1@gmail.com>
2017-12-02 17:14:26 -0600

    nav bar finalizado, falta abilitar algunos link, pero el comportamiento y la apariencia estan listo.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
-//objetos de estado
+//objetos de estado y objetos html
 estado = {homeClicks:1,}
+
+var navBarG =  '<div class="container"  >'+
+					'<div class="row">'+
+						'<div class="col-12">'+
+							'<ul>'+
+								'<li><a href="#" class="active">Home</a></li>'+
+								'<li><a href="#">About</a></li>'+
+								'<li><a href="#">Portfolio</a></li>'+
+								'<li><a href="#">Contact</a></li>'+
+								'<li><a href="#">CV</a></li>'+
+							'</ul>'+
+						'</div>'+
+					'</div>'+
+				'</div>'; 
+
+
+var navBarP =   '<div class="row">' +
+					'<div class="col-12">' +
+						'<ul>' +
+							'<li ><a href="#home" class="active">Home</a></li>' +
+							'<li class="dropdown">' +
+								'<a href="#" class="dropbtn">+</a>' +
+								'<div class="dropdown-content">' +
+									'<a href="#">About</a>' +
+									'<a href="#">Portfolio</a>' +
+									'<a href="#">Contact</a>' +
+									'<a href="#">CV</a>' +
+								'</div>' +
+							'</li>' +
+						'</ul>' +
+					'</div>' +
+				'</div>';
+
+				
+
+
 
 //funciones para el manejo del estado
 function AumentarHomeClicks(estado){
@@ -7,11 +43,19 @@ function AumentarHomeClicks(estado){
 }
 
 //funcioens para el manejo del domm
-
+function renderNavBar(navbar){
+	$("header").html(navbar);
+}
 
 //funciones de accion
 $(function(){
 	$("#mensaje").css("margin-top", ($(window).height() / 2) - 100);
+	
+	if ($(window).width() <= 460){
+		renderNavBar(navBarP);
+	}else{
+		renderNavBar(navBarP);
+	}
 });
 
 $("header").hover(function(){
@@ -20,25 +64,48 @@ $("header").hover(function(){
 	$(this).removeClass("hover");
 });
 
-$("li a").hover(function(){
-	if( !($(this).html() === "Home")){
-		$(this).addClass("not-allowed")
-		$("#no-disponible").show();
-	}
-},function(){
-	$(this).removeClass("not-allowed")
-	$("#no-disponible").hide();
+$("header").on({
+	mouseenter: function(){
+		if( !($(this).html() === "Home")  && !($(this).html() === "+") ){
+			$(this).addClass("not-allowed")
+			$("#no-disponible").show();
+		}
+	}, 
+	mouseleave: function(){
+		$(this).removeClass("not-allowed")
+		$("#no-disponible").hide();
+	},
+	click: function(){
+		if( $(this).html() === "Home"){
+			//$("li a").removeClass("active");
+			//$(this).addClass("active");
+			AumentarHomeClicks(estado);
+			if(estado.homeClicks == 5){
+				$("#yariela").fadeIn(2000, "swing");	
+			}
+		}
+	}	
+}, "li a");
 
+$("header").on({
+	mouseenter: function(){
+		$(".dropdown-content").show();		
+	}, 
+	mouseleave: function(){
+		$(".dropdown-content").hide();
+	},
+	click: function(){
+		$(".dropdown-content").toggle();
+
+	}	
+}, ".dropdown");
+
+
+
+$(window).resize(function() {
+	if ($(window).width() <= 460){
+		renderNavBar(navbarP);
+	}else{
+		renderNavBar(navBarP);
+	}
 });
-
-$("li a").click(function(){
-	if( $(this).html() === "Home"){
-	$("li a").removeClass("active");
-	$(this).addClass("active");
-	AumentarHomeClicks(estado);
-	if(estado.homeClicks == 5){
-		$("#yariela").fadeIn(2000, "swing");	
-	}
-	}
-});
-
