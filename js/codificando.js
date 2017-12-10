41f4d684f4f0f1be1b22056e69aed0e6953b7908
Lito Soler <lito.soler1@gmail.com>
2017-12-10 15:02:47 -0600

    cambios menores

--- js/codificando.js
+++ js/codificando.js
@@ -1,15 +1,9 @@
 //funciones de accion
-$(function(){
-	$("#fondo").css("margin-top", ($(window).height()-$("#fondo").innerHeight() - 10)/ 2);
-	$('body').hide();
-
-	// start up after 2sec no matter what
-    setTimeout(function(){
-    	$('html').removeClass("loading");
-    	$("body").show(600);
-    },1000);
-});
 
 $(window).resize(function() {
-	$("#fondo").css("margin-top", ($(window).height()-$("#fondo").innerHeight() - 10)/ 2);
+	autoMargin();
 });
+
+function autoMargin(){
+$("#fondo").css("margin-top", ($(window).height()-$("#fondo").innerHeight() - 10)/ 2);
+}
